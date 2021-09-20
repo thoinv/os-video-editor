9ba547e7dd72f839a54003b88a587440ca8da903
Thoi Nguyen <thoinv1510@gmail.com>
2021-09-20 17:14:26 +0700

    update main edit activity

--- app/src/main/java/com/example/videoeditor/feature/edit/EditActivity.java
+++ app/src/main/java/com/example/videoeditor/feature/edit/EditActivity.java
@@ -3,9 +3,12 @@ package com.example.videoeditor.feature.edit;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.viewbinding.ViewBinding;
 
+import com.example.videoeditor.R;
 import com.example.videoeditor.base.viewbinding.BaseActivityBinding;
 import com.example.videoeditor.databinding.ActivityEditBinding;
 
@@ -17,6 +20,50 @@ public class EditActivity extends BaseActivityBinding<ActivityEditBinding> {
 
     @Override
     protected void bindViewClickEvent() {
+        binding.layoutToolbar.btToolbarBack.setOnClickListener(v -> finish());
+        binding.btPlay.setOnClickListener(v -> onButtonPlayClicked());
+        View.OnClickListener bottomMenuOnClickListener = v -> {
+            int id = v.getId();
+            if (id == R.id.bottom_menu_edit) {
+                onBottomMenuEditClicked();
+
+            } else if (id == R.id.bottom_menu_effect) {
+                onBottomMenuEffectClicked();
+
+            } else if (id == R.id.bottom_menu_music) {
+                onBottomMenuMusicClicked();
+
+            } else if (id == R.id.bottom_menu_theme) {
+                onBottomMenuThemeClicked();
+            }
+        };
+        binding.bottomMenuEdit.setOnClickListener(bottomMenuOnClickListener);
+        binding.bottomMenuEffect.setOnClickListener(bottomMenuOnClickListener);
+        binding.bottomMenuMusic.setOnClickListener(bottomMenuOnClickListener);
+        binding.bottomMenuTheme.setOnClickListener(bottomMenuOnClickListener);
+    }
+
+    private void onButtonPlayClicked() {
+        Toast.makeText(this, "onButtonPlayClicked", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void onBottomMenuThemeClicked() {
+        Toast.makeText(this, "onBottomMenuThemeClicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onBottomMenuMusicClicked() {
+        Toast.makeText(this, "onBottomMenuMusicClicked", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void onBottomMenuEffectClicked() {
+        Toast.makeText(this, "onBottomMenuEffectClicked", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void onBottomMenuEditClicked() {
+        Toast.makeText(this, "onBottomMenuEditClicked", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -27,6 +74,6 @@ public class EditActivity extends BaseActivityBinding<ActivityEditBinding> {
 
     @Override
     protected void initViews(Bundle bundle) {
-
+        binding.layoutToolbar.tvToolbarTitle.setText(R.string.edit);
     }
 }
